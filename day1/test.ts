3ab86007ac612da91b174bb8c8b7fb8e7cf45a03
Rilr20 <nrrl99@gmail.com>
2024-12-01 15:02:44 +0100

    created template to be used

--- day1/test.ts
+++ day1/test.ts
@@ -6,7 +6,7 @@ import main from "./main";
 const { part1, part2 } = main
 
 
-function getFile(filepath:string) {
+function getFile(filepath: string) {
     try {
         return fs.readFileSync(filepath).toString();
     } catch (error) {
@@ -18,8 +18,8 @@ function part_1_test_example_1() {
     let file = getFile('example.txt');
     let result = part1(file);
     const expected = -1
-    console.log("Part 1 returned " + result);
-    result === expected ? console.log("PASSED: part 2 returned " + result) : console.log("FAILED: part 2 returned " + result + " instead of " + expected);
+    console.log("Part 1 Example returned " + result);
+    result === expected ? console.log("PASSED: part 1 Example returned " + result) : console.log("FAILED: part 1 Example returned " + result + " instead of " + expected);
 
 }
 
@@ -33,22 +33,25 @@ function part_2_test_example_1() {
     let file = getFile('example.txt');
     let result = part1(file);
     const expected = -1
-    console.log("Part 1 returned " + result);
-    result === expected ? console.log("PASSED: part 2 returned " + result) : console.log("FAILED: part 2 returned " + result + " instead of " + expected);
+    console.log("Part 2 Example returned " + result);
+    result === expected ? console.log("PASSED: part 2 Example returned " + result) : console.log("FAILED: part 2 Example returned " + result + " instead of " + expected);
 
 }
 
 function part_2_test_real_input() {
     let file = getFile('input.txt');
     let result = part1(file);
-    console.log("Part 1 returned " + result);
+    console.log("Part 2 returned " + result);
 }
 
 function run() {
     part_1_test_example_1()
+    console.log();
     part_1_test_real_input()
+    console.log();
 
     part_2_test_example_1()
+    console.log();
     part_2_test_real_input()
 }
 
